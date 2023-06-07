e4e2c61e9b78d50799483d1918efef91d0452d46
ESC18 <ponderosaprintingus@gmail.com>
2023-06-07 14:10:42 -0700

    Final commit

--- js/script.js
+++ js/script.js
@@ -1,25 +1,21 @@
 window.onload = function () {
-    let vowelAr = ["a", "e", "i", "o", "u"];
+    vowelAr = ["a", "e", "o", "u", "i"]
     let form = document.querySelector("form");
     let div = document.getElementById("output");
     form.addEventListener("submit", submitFunction);
-    let newText;
-    
+
+
     function submitFunction(event) {
-      event.preventDefault();  
-      let inputText = document.getElementById("textinput").value;
+        event.preventDefault();
+        let inputText = document.getElementById("textinput").value;
+        let savedText = inputText;
+
         for (index = 0; index < 6; index++) {
-          if (inputText[index] === "o") {
-            console.log("vowel text")
-          } else {
-            console.log('no vowels')
-          }
-            div.innerHTML = inputText;
+            savedText = savedText.replaceAll(vowelAr[index],'-');
+            div.innerHTML = savedText;
         }
     }
-    
+
+
 }
 
-function removeVowels(str) {
-    return str.replace(/[aeiou]/gi, '');
-}
